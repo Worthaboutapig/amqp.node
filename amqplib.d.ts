ed79a1db35947ca4d034da37f91af1a5b7a1160b
Jeremy Clarke <jeremy@savetrees.co.uk>
2024-07-24 11:12:00 +0100

    Update type defs

--- amqplib.d.ts
+++ amqplib.d.ts
@@ -11,26 +11,14 @@ declare module "@worthaboutapig/amqplib" {
   }
 
   export interface IChannel extends CommonChannelConnection {
-    handlers?: ChannelEventHandlers;
     assertQueue(name: string, options: unknown): Promise<void>;
     consume(name: string, handler: (message: IMessage) => Promise<void>, options: { noAck: boolean }): Promise<void>;
     sendToQueue(name: string, data: unknown, options: { persistent: true }): boolean;
   }
 
   export interface IConnection extends CommonChannelConnection {
-    handlers?: ConnectionEventHandlers;
     createChannel(): Promise<IChannel>;
   }
-
-  export type ConnectionEventHandlers = {
-    onConnectionError(error: Error): Promise<void>;
-    onConnectionClose(): Promise<void>;
-  };
-
-  export type ChannelEventHandlers = {
-    onChannelError(error: Error): Promise<void>;
-    onChannelClose(): Promise<void>;
-  };
 
   export function connect(url: string, connOptions: unknown): Promise<IConnection>;
 }
